32f5eae81197bb8e2f4fa31742df6d05d2534c51
chanchimetro <48773442+chanchimetro@users.noreply.github.com>
2023-08-21 16:04:34 -0300

    fix

--- Server/index.js
+++ Server/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from 'cors';
+import AuthServices from './services/authServices.js'
 
 const app = express();
 const port = 3000;
@@ -11,5 +12,12 @@ app.listen(port, () => {
 })
 
 app.post('/login', async (req, res) => {
-    
+    try {
+        let r = await AuthServices.login(req.body)
+        r ? console.log('OK') : console.log('NOPE');
+        res.status(200).json({message: 'Login executed!'});
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({error:  'Authentication failed!'});
+    }
 })
--- Server/services/authServices.js
+++ Server/services/authServices.js
@@ -1,10 +1,18 @@
-export class authServices {
-    static login = async() => {
+import config from '../dbconfig.js';
+import sql from 'mssql';
+
+export default class authServices {
+    static login = async(input) => {
         let r = null;
         try {
-
-        }catch {
-            
+            let pool = await sql.connect(config);
+            let result = await pool.request()
+                .input('pUser', sql.VarChar, input.user)
+                .input('pPass', sql.VarChar, input.pass)
+                .query('SELECT * FROM Users WHERE pPass == Users.password AND pUser == Users.username');
+            returnEntity = result.recordsets[0][0];
+        }catch(error) {
+            console.log(error);
         }
         return r;
     }
